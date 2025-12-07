6ee808415a49cc37bab7c8507e533ae18bbfab23
Arthur Vincent Simon <developarvin@gmail.com>
2025-12-07 13:59:43 +0800

    Reset base

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/west-coast-swing-tools/',
+  base: '/',
   plugins: [
     devtools(),
     tanstackRouter({
